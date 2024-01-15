2c9c91b08e11d63519e6029fe2291079990914e6
Sophia Zhao <90772792+sophizhao@users.noreply.github.com>
2024-01-14 20:22:07 -0500

    Commented code for readability

--- scrape.py
+++ scrape.py
@@ -1,4 +1,4 @@
-# import requests
+# Import requests
 import time 
 from bs4 import BeautifulSoup
 from selenium import webdriver 
@@ -24,12 +24,11 @@ def send_to_firestore(data):
     data['timestamp'] = datetime.now()
     doc_ref.set(data)
 
- 
+# Create a dictionary to hold all the data
 variables = ['NAME', 'RANGE', 'ROUND-TRIP LIGHT TIME', 'NAME', 'AZIMUTH', 'ELEVATION', 'WIND SPEED', 'MODE', 'SOURCE', 'FREQUENCY BAND', 'DATA RATE', 'POWER RECEIVED']
-
 res = dict.fromkeys(variables, 0)
 
-
+#DSN Website to scrape
 URL = "https://eyes.nasa.gov/dsn/dsn.html"
  
 driver = webdriver.Chrome(service=ChromeService( 
@@ -40,25 +39,30 @@ driver.implicitly_wait(3)
 driver.get(URL) 
 time.sleep(3)
 
-
+# Build list of small dishes
 small_dishes = driver.find_elements(By.CSS_SELECTOR, "div[class*='figure small_dish'")
 
 for i in range(3):
     # --------------------------------- first row -------------------------------- #
     if i == 0:
         for j in range(5):
-            temporary = "spacecraft_" + str(i) + "_" + str(j+1) #this should look like "spacecraft_0_1"
+	    # Creating an ID to search: this should look like "spacecraft_0_1"
+            temporary = "spacecraft_" + str(i) + "_" + str(j+1)
             try:
                 spacecraft_div = small_dishes[i+j].find_element(By.CSS_SELECTOR, f"div[id*={temporary}]")
                 actual_title = spacecraft_div.find_elements(By.CSS_SELECTOR, f"a[id*={temporary}]")
 
-                for item in range(len(actual_title)): #if there are multiple items in the list
+		#Iterates through if there are >1 spacecrafts on one dish
+                for item in range(len(actual_title)): 
                     if actual_title[item].accessible_name == "APM1":
+			# Click if not already selected, happens when APM1 is not first active
                         if actual_title[item].get_attribute("class") != "selected":
                             driver.find_element(By.CSS_SELECTOR, f"a[id*={temporary}_{item}]").click()
+			# Click on more detail button
                         driver.find_element(By.CSS_SELECTOR, f"a[title*='more detail']").click()
                         sidebar = driver.find_element(By.CSS_SELECTOR, "div[id*='current_data']")
                         values = sidebar.find_elements(By.TAG_NAME, 'p')
+			# Print out all values, adding them to the dictionary
                         for e in range(len(values)):
                             res[variables[e]] = values[e].text
                             # print(res[variables[e]])
@@ -66,9 +70,11 @@ for i in range(3):
                         print(res)
                         break
             except:
+		# Error: Dish is inactive
                 print("No element found")
     # ----------------------------- for rows 2 and 3 ----------------------------- #
     else:
+	# Same as above, just indices are changed for other rows
         for j in range(3):
             temporary = "spacecraft_" + str(i) + "_" + str(j+1) #this should look like "spacecraft_0_1"
             try:
